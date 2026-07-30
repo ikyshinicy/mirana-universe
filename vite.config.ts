f8eca9f80886c8b0edadbdedcb33564e3043d8e1
Rizky Reranza <rranzabussines@gmail.com>
2026-07-30 16:53:41 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,11 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: process.env.FIGMA_PUBLIC_URL ? `${process.env.FIGMA_PUBLIC_URL}/` : '/',
+    base: process.env.FIGMA_PUBLIC_URL
+      ? `${process.env.FIGMA_PUBLIC_URL}/`
+      : mode === 'production'
+        ? '/mirana-universe/'
+        : '/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
